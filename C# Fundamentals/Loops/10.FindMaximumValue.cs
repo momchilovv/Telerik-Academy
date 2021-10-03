5a2a209e8244bb5b72c468ac3d9dcbdc2b0ecb76
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2021-10-03 23:58:40 +0300

    Uploading 2 solutions for C# Fundamentals - Loops.

---
+++ C# Fundamentals/Loops/10.FindMaximumValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int input = int.Parse(Console.ReadLine());
+        List<int> numbers = new List<int>();
+        int sNumbers = 0;
+
+        for (int i = 0; i < input; i++)
+        {
+            sNumbers = int.Parse(Console.ReadLine());
+            numbers.Add(sNumbers);
+        }
+        Console.WriteLine(numbers.Max());
+    }
+}
---
+++ C# Fundamentals/Loops/11.FindLargestThreeValues.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int input = int.Parse(Console.ReadLine());
+        List<int> numbers = new List<int>();
+        int currentNumber = 0;
+        List<int> largestThree = new List<int>();
+
+        for (int i = 0; i < input; i++)
+        {
+            currentNumber = int.Parse(Console.ReadLine());
+            numbers.Add(currentNumber);
+        }
+
+        for (int i = 1; i <= 3; i++)
+        {
+            largestThree.Add(numbers.Max());
+            numbers.Remove(numbers.Max());
+        }
+        Console.WriteLine($"{largestThree[0]}, {largestThree[1]} and {largestThree[2]}");
+    }
+}
