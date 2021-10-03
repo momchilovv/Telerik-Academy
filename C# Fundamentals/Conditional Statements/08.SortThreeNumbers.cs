8ea025a306a47d9a50973cdb71cb75553295eeab
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2021-10-03 16:35:31 +0300

    Uploading the rest solutions for C# Fundamentals - Conditional Statements.

---
+++ C# Fundamentals/Conditional Statements/08.SortThreeNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int thirdNumber = int.Parse(Console.ReadLine());
+
+        if (firstNumber > secondNumber && firstNumber > thirdNumber)
+        {
+            if (secondNumber > thirdNumber)
+            {
+                Console.WriteLine($"{firstNumber} {secondNumber} {thirdNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"{firstNumber} {thirdNumber} {secondNumber}");
+            }
+        }
+        else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+        {
+            if (firstNumber > thirdNumber)
+            {
+                Console.WriteLine($"{secondNumber} {firstNumber} {thirdNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"{secondNumber} {thirdNumber} {firstNumber}");
+            }
+        }
+        else if (thirdNumber > firstNumber && thirdNumber > secondNumber)
+        {
+            if (secondNumber > firstNumber)
+            {
+                Console.WriteLine($"{thirdNumber} {secondNumber} {firstNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"{thirdNumber} {firstNumber} {secondNumber}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Conditional Statements/10.ChineseZodiac.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int year = int.Parse(Console.ReadLine());
+
+        switch (year % 12)
+        {
+            case 0:
+                Console.WriteLine("Monkey"); ;
+                break;
+            case 1:
+                Console.WriteLine("Rooster");
+                break;
+            case 2:
+                Console.WriteLine("Dog");
+                break;
+            case 3:
+                Console.WriteLine("Pig");
+                break;
+            case 4:
+                Console.WriteLine("Rat");
+                break;
+            case 5:
+                Console.WriteLine("Ox");
+                break;
+            case 6: 
+                Console.WriteLine("Tiger");
+                break;
+            case 7:
+                Console.WriteLine("Hare");
+                break;
+            case 8:
+                Console.WriteLine("Dragon");
+                break;
+            case 9:
+                Console.WriteLine("Snake");
+                break;
+            case 10:
+                Console.WriteLine("Horse");
+                break;
+            case 11:
+                Console.WriteLine("Sheep");
+                break;
+        }
+    }
+}
