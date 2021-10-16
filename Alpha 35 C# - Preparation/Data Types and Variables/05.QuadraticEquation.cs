fa551f8cfdbdd8bde96a5b31707d835b3926fbf3
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2021-10-16 20:54:33 +0300

    Uploading the rest solutions for Alpha 35 C# - Preparation.

---
+++ Alpha 35 C# - Preparation/Data Types and Variables/05.QuadraticEquation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+
+        double sum, x1, x2;
+
+        sum = b * b - (4 * a * c);
+
+        if (sum >= 0)
+        {
+            x1 = (-b - Math.Sqrt(sum)) / (2 * a);
+            x2 = (-b + Math.Sqrt(sum)) / (2 * a);
+
+            if (x1 == 0)
+            {
+                x1 = 0;
+            }
+            else if (x2 == 0)
+            {
+                x2 = 0;
+            }
+
+            Console.WriteLine($"x1={x1}; x2={x2}");
+        }
+    }
+}
---
+++ Alpha 35 C# - Preparation/Data Types and Variables/06.SumOf5Numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+        int result = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            number = int.Parse(Console.ReadLine());
+            result += number;
+        }
+        Console.WriteLine(result);
+    }
+}
