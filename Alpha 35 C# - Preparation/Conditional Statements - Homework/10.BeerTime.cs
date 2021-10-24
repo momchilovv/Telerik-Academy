b1899fe3307f6ec2565c205e37eca956f1cf72f2
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2021-10-24 15:22:55 +0300

    Uploading Alpha 35 - Preparation Conditional Statements Homework.

---
+++ Alpha 35 C# - Preparation/Conditional Statements - Homework/10.BeerTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        try
+        {
+            DateTime time = DateTime.ParseExact(input, "h:mm tt", CultureInfo.InvariantCulture);
+
+            if (time >= DateTime.Parse("1:00 PM") || time <= DateTime.Parse("3:00 AM")) 
+            {
+                Console.WriteLine("beer time");
+            }
+            else
+            {
+                Console.WriteLine("non-beer time");
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("invalid time");
+        }
+    }
+}
