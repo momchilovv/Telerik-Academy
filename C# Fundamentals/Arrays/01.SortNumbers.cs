7aebda2cc9736532235a0adba325fc7edcb6690a
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2021-10-29 00:29:52 +0300

    Uploading 2 solutions for C# Fundamentals - Arrays.

---
+++ C# Fundamentals/Arrays/01.SortNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+        Console.WriteLine(string.Join(", ", numbers.OrderByDescending(n => n)));
+    }
+}
---
+++ C# Fundamentals/Arrays/02.ReverseNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string number = Console.ReadLine();
+
+        Console.WriteLine(string.Join("", number.Reverse()));
+    }
+}
