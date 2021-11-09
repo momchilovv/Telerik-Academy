aa429e800d229e49b6840ca48acd11ad077b74ac
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2021-11-09 22:27:09 +0200

    Uploading Alpha 35 - Preparation Loops Homework.

---
+++ Alpha 35 C# - Preparation/Loops - Homework/09.MatrixOfNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        int j;
+
+        for (int i = 1; i <= number; i++)
+        {
+            for (j = i; j <= number; j++)
+            {
+                Console.Write($"{j} ");
+            }
+            for (int k = i - 1; k >= 1; k--)
+            {
+                Console.Write($"{j} ");
+                j++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
